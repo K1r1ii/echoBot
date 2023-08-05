510db7f170ce1d8014b24340558928a2f5cba42d
Kirill Salnikov <99658047+K1r1ii@users.noreply.github.com>
2023-08-05 18:21:24 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+from aiogram import Bot, Dispatcher, executor, types
+
+TOKEN = '6328368591:AAH_upq4-VbW9E15cCJ0mpdEi-yVLGH6hHs'
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot=bot)
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(text=message.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
